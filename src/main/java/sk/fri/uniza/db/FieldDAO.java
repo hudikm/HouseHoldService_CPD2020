48468a674ce60f0769a4475db7cae82ebc0b28ed
Martin Hudik <hudikm@gmail.com>
2020-05-19 01:37:25 +0200

    8.0 Pridanie možnosti vymazať položku Field

--- src/main/java/sk/fri/uniza/db/FieldDAO.java
+++ src/main/java/sk/fri/uniza/db/FieldDAO.java
@@ -31,7 +31,9 @@ public class FieldDAO extends AbstractDAO<Field> {
 
 
     public Field delete(String id) {
-        return null;
+        Field field = get(id);
+        currentSession().remove(field);
+        return field;
     }
 
     public List<Field> allFields() {
